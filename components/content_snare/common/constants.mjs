0a5454d08f3e6798bf9ced1be84fb9ebecc016a5
Jorge Cortes <jacortesmahmud@gmail.com>
2024-02-29 14:42:24 -0500

    Untested components

---
+++ components/content_snare/common/constants.mjs
@@ -0,0 +1,13 @@
+const BASE_URL = "https://api.contentsnare.com";
+const VERSION_PATH = "/partner_api/v1";
+const LAST_CREATED_AT = "lastCreatedAt";
+const DEFAULT_MAX = 600;
+const WEBHOOK_ID = "webhookId";
+
+export default {
+  BASE_URL,
+  VERSION_PATH,
+  DEFAULT_MAX,
+  LAST_CREATED_AT,
+  WEBHOOK_ID,
+};
